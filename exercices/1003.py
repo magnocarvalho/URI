2d0e6b1785b83fbf8d906fe12a7d1781205dec5e
Magno Carvalho <magnocarv@hotmail.com>
2021-03-28 01:50:09 -0300

    feat(1003): Soma Simples

---
+++ exercices/1003.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Soma Simples
+# Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. 
+# A seguir escrever o valor desta variável.
+a = input()
+b = input()
+soma = int(a) + int(b)
+print("SOMA = " + str(soma))
