eb31581418f6095701e01a516ff05c001fa273ea
Magno Carvalho <magnocarv@hotmail.com>
2021-03-28 01:41:22 -0300

    feat(1002): Área do Círculo

---
+++ exercices/1002.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# import math
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+A fórmula para calcular a área de uma circunferência é: area = π . raio2. 
+Considerando para este problema que π = 3.14159:
+
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+'''
+
+
+r = input()
+q = float(r) * float(r)
+p = 3.14159 * q
+print("A=%.4f" % p)
