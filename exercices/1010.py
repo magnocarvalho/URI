47c9823e739c660582aff10dda27df2faefad154
Magno Carvalho <magnocarv@hotmail.com>
2021-03-28 02:48:27 -0300

    feat(1010): calculo simples

---
+++ exercices/1010.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Cálculo Simples
+n = int(input())
+a = int(input())
+b = float(input())
+m = int(input())
+c = int(input())
+d = float(input())
+x = float((c * d) + (a * b))
+print("VALOR A PAGAR: R$ %.2f" % x)
