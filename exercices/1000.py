ca9d2753b87a8da42c7707f299831caa28364a13
Magno Carvalho <magnocarv@hotmail.com>
2021-03-28 00:59:52 -0300

    feat(1000): complete

---
+++ exercices/1000.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+'''
+
+print("Hello World!")
